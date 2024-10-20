314482774eb97d9abf204495ecc0d0ed99a54f02
Anu9969 <sharmaanurag9969@gmail.com>
2024-10-21 01:06:07 +0530

    simple project setup done with ui

--- 1-token-launchpad-starter/src/App.jsx
+++ 1-token-launchpad-starter/src/App.jsx
@@ -1,9 +1,39 @@
-import './App.css'
-import { TokenLaunchpad } from './components/TokenLaunchpad'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { TokenLaunchpad } from './components/TokenLaunchpad';
+import {
+    WalletModalProvider,
+    WalletMultiButton,
+    WalletDisconnectButton
+} from '@solana/wallet-adapter-react-ui';
+import '@solana/wallet-adapter-react-ui/styles.css'
+
 
 function App() {
   return (
-    <TokenLaunchpad></TokenLaunchpad>
+    <div style={{width: "100vw"}}>
+    <ConnectionProvider endpoint ="https://api.devnet.solana.com" >
+      <WalletProvider wallets ={[]}>
+        <WalletModalProvider>
+          <div style={{
+            display:'flex',
+            justifyContent:'space-between',
+            padding: 20
+          }}>
+            <WalletMultiButton/>
+            <WalletDisconnectButton/>
+            
+
+          </div>
+
+
+        <TokenLaunchpad></TokenLaunchpad>
+
+
+
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+    </div>
   )
 }
 
--- 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,17 +1,29 @@
 
+
 export function TokenLaunchpad() {
+
+    const wallet = useWallet();
+
+    async function createToken(){
+        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+        const keypair = Keypair.generate();
+
+        //you create a new mint account
+    }
+
     return  <div style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        flexDirection: 'column'
+        flexDirection: 'column',
+        padding: 20
     }}>
         <h1>Solana Token Launchpad</h1>
         <input className='inputText' type='text' placeholder='Name'></input> <br />
         <input className='inputText' type='text' placeholder='Symbol'></input> <br />
         <input className='inputText' type='text' placeholder='Image URL'></input> <br />
         <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+        <button onClick={createToken} className='btn'>Create a token</button>
     </div>
 }
